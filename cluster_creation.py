7b70cbbf59a95f363d2bd6e7f391a952a011d67e
InKyoto <squ14sss@gmail.com>
2026-04-20 18:06:25 +0300

    create cluster

--- cluster_creation.py
+++ cluster_creation.py
@@ -14,17 +14,9 @@ data = {
     "cluster_name": "hrynchuk_lab10_cluster",
     "spark_version": "14.3.x-scala2.12",
     "node_type_id": "Standard_DS3_v2",
-    "num_workers": 0,
+    "num_workers": 1,
 
-    "data_security_mode": "SINGLE_USER",
-
-    "azure_attributes": {
-        "availability": "ON_DEMAND_AZURE"
-    },
-
-    "spark_conf": {
-        "spark.databricks.cluster.profile": "singleNode"
-    }
+    "data_security_mode": "SINGLE_USER"
 }
 
 response = requests.post(url, headers=headers, json=data)
