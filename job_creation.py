7287964e2702d0ae99d429638e32ab3a048de3e7
InKyoto <squ14sss@gmail.com>
2026-04-20 19:15:36 +0300

    create job

--- job_creation.py
+++ job_creation.py
@@ -12,8 +12,8 @@ headers = {
 
 data = {
     "email_notifications": {
-        "on_failure": [os.getenv("CREATOR_EMAIL")],
-        "on_success": [os.getenv("CREATOR_EMAIL")]
+        "on_failure": os.getenv("CREATOR_EMAIL"),
+        "on_success": os.getenv("CREATOR_EMAIL")
     },
     "format": "MULTI_TASK",
     "max_concurrent_runs": 1,
@@ -24,13 +24,13 @@ data = {
         "no_alert_for_skipped_runs": False
     },
     "run_as": {
-        "user_name": [os.getenv("CREATOR_EMAIL")]
+        "user_name": os.getenv("CREATOR_EMAIL")
     },
     "tasks": [
         {
         "existing_cluster_id": "0420-150638-dy9ru7jm",
         "notebook_task": {
-            "notebook_path": f"Users/{[os.getenv("WORKSPACE_EMAIL")]}/lab10/test_notebook",
+            "notebook_path": f"Users/{os.getenv("WORKSPACE_EMAIL")}/lab10/test_notebook",
             "source": "WORKSPACE"
         },
         "run_if": "ALL_SUCCESS",
